292eba27ada8e6ff6ff0abdded406519d9fb58c6
Zia Uddin <ziauddin537000@gmail.com>
2025-11-19 21:31:58 +0600

    Add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Crucial for Electron and Capacitor to find assets
+  base: isGitHubPages ? "/zenith-pos/" : "./",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
-  }
-})
+  },
+});
